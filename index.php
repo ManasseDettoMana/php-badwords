5f7d42bc3fa24b10fc92d35509f6b5e3fd67ea71
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-10-12 17:41:09 +0200

    init

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+    $paragrafo ='Do un Perc\' a \'sta bich, sta geek come una scema (Geek) <br>
+    Fumo un Russian con dentro la scena (Backwoods) <br>
+    Non portare la tua bitch al party (Bitch ass) <br>
+    Che scopiamo lei e la sua amica scema (Big 4L) <br>
+    Baby slime, Big 4L, sono il G.O.A.T. (Sono il G.O.A.T.) <br>
+    Big boss, fumo un Back\', fat ass (Fat ass) <br>
+    Prometazina fa salire il Perc\' (Perky, ahahah) <br>
+    Voglio affogare in una vasca di racks (Cash) <br>
+    LV bag piena di lean e cash (Ice, ice, ice)  <br>
+    Diecimila sui denti, se parlo, ti acceco (Crack) <br>
+    Non c\'è bisogno di mettere il flash (Brr, pow, flash, finesse) <br>
+    VVS veri, mi guardi e diventi cieco (Brr, pow, Big 4L, THB)'
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <p><?php echo $paragrafo?></p>
+    
+
+    <?php echo ($_GET['parola']) ?>
+</body>
+</html>
