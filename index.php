e5dd226e2bbb96cc6365779dbdd218e1c0e1a419
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-10-12 18:00:37 +0200

    sostituzione paragrafo

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
 <?php 
-    $paragrafo ='Do un Perc\' a \'sta bich, sta geek come una scema (Geek) <br>
-    Fumo un Russian con dentro la scena (Backwoods) <br>
-    Non portare la tua bitch al party (Bitch ass) <br>
-    Che scopiamo lei e la sua amica scema (Big 4L) <br>
-    Baby slime, Big 4L, sono il G.O.A.T. (Sono il G.O.A.T.) <br>
-    Big boss, fumo un Back\', fat ass (Fat ass) <br>
-    Prometazina fa salire il Perc\' (Perky, ahahah) <br>
-    Voglio affogare in una vasca di racks (Cash) <br>
-    LV bag piena di lean e cash (Ice, ice, ice)  <br>
-    Diecimila sui denti, se parlo, ti acceco (Crack) <br>
-    Non c\'è bisogno di mettere il flash (Brr, pow, flash, finesse) <br>
-    VVS veri, mi guardi e diventi cieco (Brr, pow, Big 4L, THB)';
+    $paragrafo ='E buonasera signore e signori <br>
+    Fuori gli attori <br>
+    Vi conviene toccarvi i coglioni <br>
+    Vi conviene stare zitti e buoni <br>
+    Qui la gente è strana tipo spacciatori)';
 
     $lunghezzaParagrafo = strlen($paragrafo);
     $censura = '***';
